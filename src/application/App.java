397f69d2f5a2dfded14f6ca03301756f3b32b2a0
Gustavo Kazuo <kazucalado@gmail.com>
2022-05-01 10:24:10 -0300

    funcionando

--- src/application/App.java
+++ src/application/App.java
@@ -10,8 +10,10 @@ public class App {
         System.out.println("Hello, World!");
         Shape s1 = new Circle(Color.WHITE, 2.0);
         Shape s2 = new Rectangle(Color.BLACK, 3.0, 4.0);
-        System.out.println(s1.getColor());
-        System.out.println(s2.getColor());
+        System.out.println("Cor do Circle:" + s1.getColor());
+        System.out.println("Cor do Circle:" + String.format("%.3f", s1.area()));
+        System.out.println("Cor do Rectangle:" + s2.getColor());
+        System.out.println("Cor do Rectangle:" + String.format("%.3f", s2.area()));
 
     }
 }
