f705346dae7218c012f240a65fca8f49b83c71dc
Gustavo Kazuo <kazucalado@gmail.com>
2022-05-01 10:31:00 -0300

    Utilizando  classe herança com interface

--- src/application/App.java
+++ src/application/App.java
@@ -1,6 +1,5 @@
 package application;
 
-import entities.Shape;
 import enums.Color;
 import entities.*;
 import entities.Rectangle;
@@ -8,8 +7,8 @@ import entities.Rectangle;
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        Shape s1 = new Circle(Color.WHITE, 2.0);
-        Shape s2 = new Rectangle(Color.BLACK, 3.0, 4.0);
+        AbstractShape s1 = new Circle(Color.WHITE, 2.0);
+        AbstractShape s2 = new Rectangle(Color.BLACK, 3.0, 4.0);
         System.out.println("Cor do Circle:" + s1.getColor());
         System.out.println("Cor do Circle:" + String.format("%.3f", s1.area()));
         System.out.println("Cor do Rectangle:" + s2.getColor());
--- src/entities/Shape.java
+++ src/entities/Shape.java
@@ -1,22 +1,7 @@
 package entities;
 
-import enums.Color;
+public interface Shape {
 
-public abstract class Shape {
-    private Color color;
-
-    public Shape(Color color) {
-        this.color = color;
-    }
-
-    public Color getColor() {
-        return color;
-    }
-
-    public void setColor(Color color) {
-        this.color = color;
-    }
-
-    public abstract double area();
+    double area();
 
 }
